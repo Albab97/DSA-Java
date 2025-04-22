819a1bce2e41e70f93179c4b3aab6fe79e216398
Albab97 <albabahmed21@yahoo.com>
2025-04-22 11:00:36 +0530

    packages

---
+++ Java/packages/a/Message.java
@@ -0,0 +1,7 @@
+package packages.a;
+
+public class Message {
+    public static void message(){
+        System.out.println("This is a secret message");
+    }
+}
---
+++ Java/packages/b/Greeting.java
@@ -0,0 +1,10 @@
+package packages.b;
+
+import static packages.a.Message.message;
+
+public class Greeting {
+    public static void main(String[] args) {
+        System.out.println("Hi there !");
+        message();
+    }
+}
