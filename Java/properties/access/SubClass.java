cd45def85c26b324de7f132092eef69d0a983335
Albab97 <albabahmed21@yahoo.com>
2025-05-05 07:56:46 +0530

    access modifier

--- Java/properties/access/SubClass.java
+++ Java/properties/access/SubClass.java
@@ -1,15 +1,33 @@
 package properties.access;
 
-public class SubClass extends A{
+import org.w3c.dom.ls.LSOutput;
 
+public class SubClass extends A{
     public SubClass(int num, String name) {
         super(num, name);
     }
-
     public static void main(String[] args) {
         SubClass obj = new SubClass(20,"Almaan");
         String name = obj.name; // able to access name because it is protected which is being accessed in SubClass which is a subclass of A.
         System.out.println(name);
 
     }
+}
+
+class SubSubClass extends SubClass{
+
+    public SubSubClass(int num, String name) {
+        super(num, name);
+    }
+    SubSubClass obj = new SubSubClass(32,"Rambo");
+//    obj.name; // able to access name because it is protected.
+}
+
+class SubClass2 extends A{
+    public SubClass2(int num, String name) {
+        super(num, name);
+    }
+    SubClass2 obj = new SubClass2(18,"Tyler");
+//    obj.name; // able to access name because it is protected.
+
 }
