c2044df95ec0ec1f0dde45b9d4979312c3adae4f
Albab97 <albabahmed21@yahoo.com>
2025-10-28 07:10:23 +0530

    changes

--- Java/Arrays/ArrayListExample.java
+++ Java/Arrays/ArrayListExample.java
@@ -1,12 +1,14 @@
 package Arrays;
 
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Scanner;
 
 public class ArrayListExample {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
-        ArrayList<Integer> list = new ArrayList<>(5); // dynamic array of variable size. We can add any no. of elements
+        List<Integer> list = new ArrayList<>(5); // dynamic array of variable size. We can add any no. of elements
         list.add(123);
         list.add(0);
         list.add(34);
@@ -18,19 +20,29 @@ public class ArrayListExample {
 
         list.set(0,99);
         System.out.println(list);
-
+        list.add(1,12);
+        System.out.println(list);
         list.remove(2);
-
+        list.remove(Integer.valueOf(99));
         System.out.println(list);
 
-        //input
-        for (int i = 0; i < 5; i++) {
-            list.add(scn.nextInt());
+//        //input
+//        for (int i = 0; i < 5; i++) {
+//            list.add(scn.nextInt());
+//        }
+//        //get item at any index
+//        for (int i = 0; i < 5; i++) {
+//            System.out.print(list.get(i)+ " ");
+//        }
+//        System.out.println(list);
+
+//        for (Integer element : list){
+//            System.out.print(element+ " ");
+//        }
+
+        Iterator<Integer> it = list.iterator();
+        while (it.hasNext()){
+            System.out.println("iterator "+it.next());
         }
-        //get item at any index
-        for (int i = 0; i < 5; i++) {
-            System.out.print(list.get(i)+ " ");
-        }
-        System.out.println(list);
     }
 }
